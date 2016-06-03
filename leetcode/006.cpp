5ba7544846f8c8b63d1fc74b25362e852822cba1
xuzhang <snakeshawnzhang@gmail.com>
2016-06-03 17:29:28 +0800

    [+]Leetcode:006.

--- leetcode/006.cpp
+++ leetcode/006.cpp
@@ -11,12 +11,36 @@
  * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
  */
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 class Solution {
 	public:
 		string convert(string s, int numRows) {
-		
+			int length = s.length();
+			if (numRows>length || numRows <= 1)
+				return s;
+			string res;
+			int step = 2*(numRows-1);
+			for (int i; i<numRows; i++){
+				int interval = step -2*i;
+
+				for (int j=i; j<length; j+=step){
+					res += s[j];
+					if(interval>0 && interval<step && j+interval<length){
+						res += s[j+interval];
+					}
+				}
+			}
+			return res;
 		}
 };
+
+int main(){
+	string str = "PAYPALISHIRING";
+	Solution s;
+	string res = s.convert(str, 3);
+	cout<<res<<endl;
+	return 0;
+}
